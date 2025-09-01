618f98743047de910896b3f480a2339707acb949
Fatima Sharifi <ftshn84@gmail.com>
2025-09-01 19:17:35 +0200

    added java script for a link page

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const openBtn = document.getElementById("openOverlay");
+const closeBtn = document.getElementById("closeOverlay");
+const overlay = document.getElementById("overlayPage");
 
+openBtn.addEventListener("click", () => {
+  overlay.style.display = "flex";
+});
+
+closeBtn.addEventListener("click", () => {
+  overlay.style.display = "none";
+});
